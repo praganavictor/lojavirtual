1990c52620d63ff8f03d648b4fd937c2bc9acbac
Victor Pragana <victormerigue@gmail.com>
2019-10-29 09:27:27 -0300

    adicionando o header para a pagina especifica que sera utilizado

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 // import { Container } from './styles';
+import Header from "../../components/Header";
 
 export default function dashboard() {
-  return <div />;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
